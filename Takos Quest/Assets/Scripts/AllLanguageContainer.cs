0e79503e1edae6745d98353ce5f4b42e9b8181b0
PolJosh <pablofranciadelbusto@gmail.com>
2018-10-19 16:59:05 -0500

    TestScript
    AllLanguageContainer Script

---
+++ Takos Quest/Assets/Scripts/AllLanguageContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllLanguageContainer : MonoBehaviour {
+
+	public int currentLanguage;
+	public ApplicateLanguageValue[] allLanguageTexts;
+	// Use this for initialization
+	void Start () {
+		currentLanguage = PlayerPrefs.GetInt ("CurrentLanguage");
+		ApplicateAllLanguageChange (currentLanguage);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ApplicateAllLanguageChange(int language){
+		currentLanguage = language;
+		for (int i = 0; i < allLanguageTexts.Length; i++) {
+			allLanguageTexts [i].ApplicateChangeLanguage (language);
+		}
+	}
+}
