a3da86c7c333fddd9832f368cbb6536593c08898
PolJosh <pablofranciadelbusto@gmail.com>
2018-10-19 19:17:51 -0500

    Second Language Script
    ApplicateLanguageValue Script

---
+++ Takos Quest/Assets/Scripts/ApplicateLanguageValue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ApplicateLanguageValue : MonoBehaviour {
+
+	public TextAsset _textAsset;
+	public string[] dialogueLines;
+	private Text _text;
+	// Use this for initialization
+	void Awake () {
+		_text = GetComponent<Text> ();
+		if (_textAsset != null) {
+			dialogueLines = _textAsset.text.Split ('\n');
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ApplicateChangeLanguage(int language){
+		_text.text = dialogueLines [language];
+	}
+}
