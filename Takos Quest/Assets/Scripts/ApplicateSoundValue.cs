57d3a43c8a93bcbdac8e0ab5397b9e44f988abcc
PolJosh <pablofranciadelbusto@gmail.com>
2018-10-20 19:12:01 -0500

    Sound script
    Applicate sound script

---
+++ Takos Quest/Assets/Scripts/ApplicateSoundValue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicateSoundValue : MonoBehaviour {
+
+	private float soundValue;
+	public string typeSoundPref;
+	public float maxSoundValue;
+	public AudioSource sourceMusic;
+	public AudioClip clipToPlay;
+	public bool canPlayClip;
+	// Use this for initialization
+	void Awake () {
+		ApplicateChange ();
+	}
+	void Start(){
+		canPlayClip = true;
+	}
+	// Update is called once per frame
+	public void ApplicateChange () {
+		soundValue = PlayerPrefs.GetFloat (typeSoundPref);
+		sourceMusic.volume = soundValue * maxSoundValue;
+	}
+	public void PlayClip(){
+		if (canPlayClip == true) {
+			sourceMusic.clip = clipToPlay;
+			sourceMusic.Play ();
+		}
+	}
+}
