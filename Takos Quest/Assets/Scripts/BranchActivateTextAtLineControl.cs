1eefa71583545dfcc2e3814cf561f5a7e7a2cfc3
Ivan Enriquez <vanci_5@hotmail.com>
2018-10-21 01:07:55 -0500

    ActiveText

---
+++ Takos Quest/Assets/Scripts/BranchActivateTextAtLineControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchActivateTextAtLineControl : MonoBehaviour {
+
+	[Header("Dialogue Lines Variables")]
+	public TextAsset[] arrayOfArchives;
+	private TextAsset actualText;
+	public int startLine;
+	public int endLine;
+	public bool isFirstDialogue;
+	public bool isFinalDialogue;
+
+	[Header("Text Box Manager Variables")]
+	public BranchTextBoxManagerControl textBoxManagerTmp;
+
+	[Header("Language Variables")]
+	public int currentLanguaje;
+
+	[Header("Girls Variables")]
+	public int portraitPoseToSend;
+	public int portraitGirlToSend;
+	public int portraitEmotionToSend;
+
+	[Header("Answers Variables")]
+	public bool haveResponse;
+	public int startAnswerText;
+	public int[] positionsToSend;
+
+	[Header("Aditiopns Functionalities Variables")]
+	public bool destroyAfterActivate;
+	public bool isAPlayerName;
+
+	void Start () {
+		ChangeLanguaje ();
+		if (isFirstDialogue) {
+			Invoke ("SendReloadTextBoxManager", 1f);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void SendReloadTextBoxManager(){
+
+		textBoxManagerTmp.isAPlayerName = isAPlayerName;
+
+		textBoxManagerTmp.ReloadScript (actualText, portraitEmotionToSend,portraitGirlToSend,portraitPoseToSend);
+		textBoxManagerTmp.currentLine = startLine;
+		textBoxManagerTmp.endAtLine = endLine;
+		textBoxManagerTmp.EnabledTextBox ();
+		textBoxManagerTmp.SetPositions (positionsToSend);
+		textBoxManagerTmp.haveResponse = haveResponse;
+
+		if (haveResponse == true) {
+			textBoxManagerTmp.SetAnswerText (startAnswerText);
+		} else if (haveResponse == false && isFinalDialogue == false){
+			textBoxManagerTmp.SetNextLinealDialogue (positionsToSend [0]);
+		}
+		if (destroyAfterActivate == true) { 
+			Destroy (this.gameObject);
+		}
+	}
+	public void ChangeLanguaje(){
+		currentLanguaje = PlayerPrefs.GetInt("CurrentLanguage");
+		switch (currentLanguaje) {
+		case 0:
+			actualText = arrayOfArchives [0];
+			break;
+		case 1:
+			actualText = arrayOfArchives [1];
+			break;
+		case 2:
+			actualText = arrayOfArchives [2];
+			break;
+		case 3:
+			actualText = arrayOfArchives [3];
+			break;
+		}
+	}
+}
---
+++ Takos Quest/Assets/Scripts/BranchAnswerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BranchAnswerControl : MonoBehaviour {
+
+	public Text optionTxt;
+	public BranchAnswersContainer answerContainer;
+	public int optionNumber;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void SetOptionText(string text){
+		optionTxt.text = text;
+	}
+	public void OptionSelected(){
+		answerContainer.optionSelected = optionNumber;
+		answerContainer.DisappearOptions ();
+	}
+}
