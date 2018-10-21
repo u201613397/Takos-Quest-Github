a0fa04094230a50e3450fa5bc841f68a78d49bca
Ivan Enriquez <vanci_5@hotmail.com>
2018-10-21 09:44:56 -0500

    Conversation

---
+++ Takos Quest/Assets/Scripts/ConversationPortraitContainer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationPortraitContainer : MonoBehaviour {
+
+	public ConversationPortraitControl[] allPosiblePortraits;
+	public int currentGirlSelected = -1;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void ChangePortrait(int numbOfGirl, int numbOfPos, int numbOfAnimation){
+		if (currentGirlSelected != numbOfGirl) {
+			allPosiblePortraits [currentGirlSelected].DisappearPortrait ();
+		}
+		currentGirlSelected = numbOfGirl;
+		allPosiblePortraits [numbOfGirl].AppearPortrait (numbOfPos, numbOfAnimation);
+	}
+}
+
---
+++ Takos Quest/Assets/Scripts/ConversationSoundsControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationSoundsControl : MonoBehaviour {
+
+	public AudioClip[] arrayOfClips;
+	private AudioSource clipSource;
+	// Use this for initialization
+	void Start () {
+		clipSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void PlayClip(int clipSelected){
+		if (clipSource.isPlaying) {
+			clipSource.Stop ();
+		}
+		clipSource.clip = arrayOfClips[clipSelected];
+		clipSource.Play ();
+	}
+}
